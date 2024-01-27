3ff1bf135d168f9856258d5a5fa0dcce48b0d993
Claudio <carnevale.claudio1@gmail.com>
2024-01-27 17:37:31 +0100

    icons, font, header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFacebookF, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from "./components/header/header.component";
@@ -9,8 +11,11 @@ import { HeaderComponent } from "./components/header/header.component";
     standalone: true,
     templateUrl: './app.component.html',
     styles: '',
-    imports: [RouterOutlet, HeaderComponent, FooterComponent, NgbNavModule]
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, NgbNavModule, FontAwesomeModule]
 })
 export class AppComponent {
+    twitter = faTwitter;
+    facebook = faFacebookF;
+    linkedin = faLinkedinIn;
 
 }
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import dayjs from 'dayjs';
 
 @Component({
     selector: 'w-footer',
     standalone: true,
-    imports: [],
+    imports: [FontAwesomeModule],
     templateUrl: './footer.component.html',
     styles: ``
 })
 export class FooterComponent {
     currentYear = dayjs().year();
+
+    facebook = faFacebookF;
+    instagram = faInstagram;
 }
