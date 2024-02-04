ab4261f9409505f309c381fc81b55b61b29851b6
Claudio <carnevale.claudio1@gmail.com>
2024-02-04 10:51:28 +0100

    animate.css impl

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 /// <reference types="@angular/localize" />
 
 import { bootstrapApplication } from '@angular/platform-browser';
-import 'animate.css';
+// import 'animate.css';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
